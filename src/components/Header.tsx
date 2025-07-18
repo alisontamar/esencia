f5344d8a7dc3264fd87a9bc47b036b916cf6428c
rodriknd <rp752989@gmail.com>
2025-07-18 12:42:33 -0400

    Agregar tipo de entrada "search" en los campos de búsqueda del encabezado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -93,6 +93,7 @@ export const Header = () => {
             <div className="relative">
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
               <Input
+                type="search"
                 placeholder="Buscar productos..."
                 value={searchQuery}
                 onChange={(e) => {
@@ -194,6 +195,7 @@ export const Header = () => {
               <div className="relative">
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
                 <Input
+                  type="search"
                   placeholder="Buscar productos..."
                   onChange={(e) => {
                   setSearchQuery(e.target.value);
