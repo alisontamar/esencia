11141a9a42d13711ae3ecfb98040bc531996c752
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2025-08-05 17:55:37 -0400

    Updating contact message

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,11 +4,11 @@ export const Contact = () => {
     return (
         <section className="p-3 mx-3 md:mx-0 flex-wrap bg-gradient-to-r from-pink-500 text-white to-purple-600 flex md:justify-between justify-center gap-3 md:gap-0 items-center shadow-md rounded-lg mt-6">
             <p>
-                Catálogo Virtual, si te interesa, puedes contactarme.
+                Si vendes cosméticos o productos del hogar, esto es para ti. Tu propio catálogo online. ¿Te interesa?
             </p>
-            <button 
-            className="bg-pink-400 hover:bg-pink-500 text-white font-bold py-2 px-4 rounded"
-            onClick={handleWhatsAppClick}>
+            <button
+                className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded"
+                onClick={handleWhatsAppClick}>
                 Contactar
             </button>
         </section>
