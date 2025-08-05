bee918ae08ab536987478cfaf4387b270f172b04
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2025-08-05 18:33:56 -0400

    Implementing announcement messages

---
+++ src/components/AnnounceFeatures.tsx
@@ -0,0 +1,10 @@
+export const AnnounceFeatures = () => {
+    return (
+        <div className="container mt-4">
+            <div className="text-xl font-semibold text-pink-600 py-2 px-4">
+                🌟 ¡Actualizaciones exclusivas en camino! Únete y aprovecha antes que nadie 🌟
+            </div>
+        </div>
+    );
+}
+
