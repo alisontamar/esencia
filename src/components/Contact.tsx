e3841978abb68cd15193014ebc3288a7dd2d20f3
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2025-08-05 18:34:15 -0400

    Changing contact message

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,9 +2,9 @@ import { handleWhatsAppClick } from "@/lib/utils";
 
 export const Contact = () => {
     return (
-        <section className="p-3 mx-3 md:mx-0 flex-wrap bg-gradient-to-r from-pink-500 text-white to-purple-600 flex md:justify-between justify-center gap-3 md:gap-0 items-center shadow-md rounded-lg mt-6">
+        <section className="p-3 mx-3 md:mx-0 flex-wrap bg-gradient-to-r from-pink-500 text-white to-purple-600 flex md:justify-between justify-center gap-3 md:gap-0 items-center shadow-md rounded-lg mt-4">
             <p>
-                Si vendes cosméticos o productos del hogar, esto es para ti. Tu propio catálogo online. ¿Te interesa?
+                ¿Vendes cosméticos o productos del hogar? Multiplica tus ventas con tu catálogo online. ¡Escríbeme ya!
             </p>
             <button
                 className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded"
