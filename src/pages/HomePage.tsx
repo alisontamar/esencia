63818265d7998dc95a0b986c76ac45665c6bce1d
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2025-08-05 18:34:34 -0400

    Importing AnnounceFeatures component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,10 +3,12 @@ import { BrandCarousel } from '@/components/BrandCarousel';
 import { BestSellerSection } from '@/components/BestSellerSection';
 import { CategoryGrid } from '@/components/CategoryGrid';
 import { FeaturedProducts } from '@/components/FeaturedProducts';
+import { AnnounceFeatures } from '@/components/AnnounceFeatures';
 
 export const HomePage = () => {
   return (
     <div>
+      <AnnounceFeatures />
       <HeroSection />
       <BrandCarousel />
       <BestSellerSection />
