25858a30c9901807f6b78294edd802060fc02009
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2025-09-01 11:45:13 -0400

    Remove AnnounceFeatures component from HomePage for a cleaner layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,12 +3,10 @@ import { BrandCarousel } from '@/components/BrandCarousel';
 import { BestSellerSection } from '@/components/BestSellerSection';
 import { CategoryGrid } from '@/components/CategoryGrid';
 import { FeaturedProducts } from '@/components/FeaturedProducts';
-import { AnnounceFeatures } from '@/components/AnnounceFeatures';
 
 export const HomePage = () => {
   return (
     <div>
-      <AnnounceFeatures />
       <HeroSection />
       <BrandCarousel />
       <BestSellerSection />
