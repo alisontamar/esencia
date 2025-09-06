7792c7e0f1c49db7cb073488069aee8febf5f3c6
RodrigoKND <84539101+RodrigoKND@users.noreply.github.com>
2025-09-06 16:48:49 -0400

    Adding react-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,33 @@ import { CatalogPage } from '@/pages/CatalogPage';
 import { ProductPage } from '@/pages/ProductPage';
 import { BrandPage } from '@/pages/BrandPage';
 import ProductsProvider from '@/contexts/ProductsContext';
+import { HelmetProvider } from 'react-helmet-async';
 import './App.css';
 
 function App() {
   return (
-    <ProductsProvider>
-      <Router>
-        <div className="min-h-screen">
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/catalog" element={<CatalogPage />} />
-              <Route path="/product/:id" element={<ProductPage />} />
-              <Route path="/brand/:name" element={<BrandPage />} />
-            </Routes>
-          </main>
-          <Footer />
-          <Analytics mode="production" />
-          <SpeedInsights />
-          <Toaster position="top-right" reverseOrder={false} />
-        </div>
-      </Router>
-    </ProductsProvider>
+    <HelmetProvider>
+      <ProductsProvider>
+        <Router>
+          <div className="min-h-screen">
+            <Header />
+            <main>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/catalog" element={<CatalogPage />} />
+                <Route path="/catalog/category/:category" element={<CatalogPage />} />
+                <Route path="/product/:id" element={<ProductPage />} />
+                <Route path="/brand/:name" element={<BrandPage />} />
+              </Routes>
+            </main>
+            <Footer />
+            <Analytics mode="production" />
+            <SpeedInsights />
+            <Toaster position="top-right" reverseOrder={false} />
+          </div>
+        </Router>
+      </ProductsProvider>
+    </HelmetProvider>
   );
 }
 
